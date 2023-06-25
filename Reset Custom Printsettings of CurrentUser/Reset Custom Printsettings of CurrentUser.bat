4e7bb9f6ff795b65d3b5ed4baf0000b2b550c806
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-06-25 16:48:51 +0200

    Update Reset Custom Printsettings of CurrentUser.bat

--- Reset Custom Printsettings of CurrentUser/Reset Custom Printsettings of CurrentUser.bat
+++ Reset Custom Printsettings of CurrentUser/Reset Custom Printsettings of CurrentUser.bat
@@ -5,6 +5,6 @@ CD /D "%~dp0"
 
 TITLE Reset Custom Printsettings of CurrentUser
 
-for /F "tokens=*" %%A IN ('REG QUERY HKCU\Printers\Connections') do ( REG DELETE %%A /v DevMode /f )
+for /F "tokens=*" %%A IN ('REG QUERY HKCU\Printers\Connections') do ( REG DELETE %%A /v DevMode /f ) >NUL 2>NUL
 
 pause
