bc3822487386ef0a9fd55f07027c76d4869fd903
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-01-10 15:41:34 +0100

    Update files

---
+++ script for easy installation of windows updates as file/msu update Installer.bat
@@ -0,0 +1,33 @@
+@echo off
+
+echo Checking for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+echo Permission check result: %errorlevel%
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges...
+goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+echo Running created temporary "%temp%\getadmin.vbs"
+timeout /T 2
+"%temp%\getadmin.vbs"
+exit /B
+
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0" 
+
+echo Batch was successfully started with admin privileges
+echo .
+cls
+(for %%a IN (*.msu) DO echo %%a && wusa %%a /quiet /norestart )
+pause
+exit
