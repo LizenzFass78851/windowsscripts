e0a39691bfe1dc4442521e85d836b1dac9ddbb49
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-03-02 17:18:24 +0100

    Upload get admin examples

---
+++ get admin script/get-new-admin.bat
@@ -0,0 +1,40 @@
+@echo off
+
+echo Checking for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+echo Permission check result: %errorlevel%
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges...
+goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+if exist "%SYSTEMROOT%\System32\Cscript.exe" ( goto VBS )
+goto PS
+
+:VBS
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+echo Running created temporary "%temp%\getadmin.vbs"
+timeout /T 2
+"%temp%\getadmin.vbs"
+exit /B
+
+:PS
+REM timeout /T 2
+powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
+exit /b
+
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+
+echo Batch was successfully started with admin privileges
+echo .
+cls
+cmd
