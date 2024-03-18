81808cb1ad751f2cc3ddde8559e3dbf2ce145c54
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-03-18 16:31:14 +0100

    Fix get-admin phase again

--- get admin script/get-new-admin-ng.bat
+++ get admin script/get-new-admin-ng.bat
@@ -14,17 +14,4 @@ if exist "%SYSTEMROOT%\System32\Cscript.exe" (
 if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs"
 pushd "%CD%" && CD /D "%~dp0"
 cls
-TITLE Nur das alte Kontextmenü aufrufen
-ECHO Bereit?
-echo 1 Nein
-echo 2 Ja
-set /p uni= Option in Zahl eintippen:
-if %uni% ==1 goto :exits
-if %uni% ==2 goto :runs
-
-:runs
-reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
-
-:exits
-timeout 10
-exit
+cmd
--- script for easy installation of windows updates as file/cab update Installer (self destruct mode).bat
+++ script for easy installation of windows updates as file/cab update Installer (self destruct mode).bat
@@ -1,41 +1,18 @@
 @echo off
 
-echo Checking for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-echo Permission check result: %errorlevel%
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-echo Requesting administrative privileges...
-goto UACPrompt
-) else ( goto gotAdmin )
-
+>nul 2>&1 fsutil dirty query %systemdrive% && (goto gotAdmin) || (goto UACPrompt)
 :UACPrompt
-if exist "%SYSTEMROOT%\System32\Cscript.exe" ( goto VBS )
-goto PS
-
-:VBS
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-echo Running created temporary "%temp%\getadmin.vbs"
-timeout /T 2
-"%temp%\getadmin.vbs"
-exit /B
-
-:PS
-REM timeout /T 2
-powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
-exit /b
-
+if exist "%SYSTEMROOT%\System32\Cscript.exe" (
+    echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "%~s0", "", "", "runas", 1 > "%temp%\getadmin.vbs"
+    cscript //nologo "%temp%\getadmin.vbs"
+    exit /b
+) else (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%~s0'"
+    exit /b
+)
 :gotAdmin
-if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-pushd "%CD%"
-CD /D "%~dp0"
-
-echo Batch was successfully started with admin privileges
-echo .
+if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs"
+pushd "%CD%" && CD /D "%~dp0"
 cls
 dism /online /add-package /norestart /packagepath:.\
 
