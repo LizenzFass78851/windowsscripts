4d1e41fbbcd9b7b99190fef64109977bbab69f87
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-04-27 06:31:19 +0000

    refactor: improve context menu script

--- w11 modding/Windows 11 - enable old context menu.bat
+++ w11 modding/Windows 11 - enable old context menu.bat
@@ -24,6 +24,7 @@ if %uni% ==2 goto :runs
 
 :runs
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+taskkill /IM explorer.exe /F && start explorer.exe
 
 :exits
 timeout 10
