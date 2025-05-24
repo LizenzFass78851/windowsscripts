3e03dcdd9c24b0a20db6fa3027fe47bf49c287f0
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-05-24 13:35:46 +0200

    refactor(run DVD with VLC): improvement

--- run DVD with VLC Media Player/run DVD with VLC Media Player.bat
+++ run DVD with VLC Media Player/run DVD with VLC Media Player.bat
@@ -1,3 +1,22 @@
 @echo off
- for %%I in (D E F G H I J K L M N O P Q R S T U V W X Y Z) do if exist "%%I:\\VIDEO_TS" set setupdrv=%%I
- "C:\Program Files\VideoLAN\VLC\vlc.exe" dvd:///%setupdrv%:/
+color 0c
+
+for %%I in (D E F G H I J K L M N O P Q R S T U V W X Y Z) do if exist "%%I:\\VIDEO_TS" set setupdrv="%%I:/" && goto :done
+goto :fail
+
+:fail
+echo No DVD detected
+echo Please try again
+timeout 5
+exit
+
+:error
+echo Error occurred while executing the script
+echo Please check the script
+timeout 10
+exit
+
+:done
+cd "C:\Program Files\VideoLAN\VLC\" || goto :error
+start vlc.exe dvd:///%setupdrv%
+exit
