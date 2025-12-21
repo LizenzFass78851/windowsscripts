f37d2e58a8a67f81178b47f8ea17128149eec690
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-12-21 19:49:33 +0100

    Add RemoveAppsScriptAsLoginScript on w11 modding
    This script can be used in Active Directory as a login script to remove selected apps when the current user logs in. Unfortunately, some other methods and Group Policy Objects (GPOs) for removing apps for the current user in Active Directory currently only work for enterprise or education systems.

---
+++ w11 modding/RemoveAppsScriptAsLoginScript.ps1
@@ -0,0 +1,45 @@
+﻿$appsToRemove = @(
+    "Clipchamp.Clipchamp",
+    "Microsoft.BingNews",
+    "Microsoft.BingWeather",
+    "Microsoft.Copilot",
+    "Microsoft.GamingApp",
+    "Microsoft.MicrosoftOfficeHub",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.MicrosoftStickyNotes",
+    "Microsoft.OutlookForWindows",
+    "Microsoft.Paint",
+    "Microsoft.ScreenSketch",
+    "Microsoft.Todos",
+    "Microsoft.Windows.Photos",
+    "Microsoft.WindowsCalculator",
+    "Microsoft.WindowsCamera",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.WindowsNotepad",
+    "Microsoft.WindowsSoundRecorder",
+    "Microsoft.WindowsTerminal",
+    "Microsoft.Xbox.TCUI",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.XboxSpeechToTextOverlay",
+    "Microsoft.ZuneMusic",
+    "MicrosoftCorporationII.QuickAssist",
+    "MSTeams"
+)
+
+Write-Host "Starting removimg of selected apps..." -ForegroundColor Cyan
+
+foreach ($app in $appsToRemove) {
+    Write-Host "`nTry to remove: $app ..." -ForegroundColor Yellow
+
+    Get-AppxPackage -Name $app |
+        Remove-AppxPackage -ErrorAction SilentlyContinue
+
+    if ($?) {
+        Write-Host "-> Removed: $app" -ForegroundColor Green
+    } else {
+        Write-Host "-> Not found or not removable: $app" -ForegroundColor Red
+    } 
+}
+
+Write-Host "`nDone!" -ForegroundColor Cyan
+
